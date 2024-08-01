a660cb1cfa02d8697b8ef1746011138e6ebe60a9
phlakhani <piyushlakhani.net@gmail.com>
2024-07-31 21:30:46 -0400

    Portfolio-v0.2 - Fixed Typos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /* -------------------------------------------
-Author:		Nazar Miller
+Author:		NCT
 ------------------------------------------- */
 $(function () {
 
@@ -252,22 +252,22 @@ $(function () {
 
     bar.animate(1);
 
-    var bar = new ProgressBar.Line(lineprog7, {
-        strokeWidth: 1.72,
-        easing: 'easeInOut',
-        duration: 1400,
-        delay: 3200,
-        trailWidth: 1.72,
-        svgStyle: {
-            width: '100%',
-            height: '100%'
-        },
-        step: (state, bar) => {
-            bar.setText(Math.round(bar.value() * 100) + ' %');
-        }
-    });
+    // var bar = new ProgressBar.Line(lineprog7, {
+    //     strokeWidth: 1.72,
+    //     easing: 'easeInOut',
+    //     duration: 1400,
+    //     delay: 3200,
+    //     trailWidth: 1.72,
+    //     svgStyle: {
+    //         width: '100%',
+    //         height: '100%'
+    //     },
+    //     step: (state, bar) => {
+    //         bar.setText(Math.round(bar.value() * 100) + ' %');
+    //     }
+    // });
 
-    bar.animate(1);
+    // bar.animate(1);
 
     // Contact form
     $('.art-input').keyup(function () {
